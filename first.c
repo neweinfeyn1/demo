3d2062ae1c178cd75a6d2b3cbc3a3dee66738f37
Kofi <cruelladevyll@gmail.com>
2017-09-18 00:44:19 -0300

    all files

--- first.c
+++ first.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 
- struct car
-{
-   int miles;
-   char *name;
 
-
-};
-
-
+ 
 
 
 
@@ -18,10 +12,27 @@
 int main()
 
 {
+const float DENSITY=62.4;
+float weight, volume;
+int size, letters;
+char *name; // name is an array of 40 chars
+name=malloc(256);
+printf("Hi! What's your first name?\n");
+scanf("%s", name);
+printf("%s, what's your weight in pounds?\n", name);
+scanf("%f", &weight);
+size = sizeof name;
+letters = strlen(name);
+volume = weight / DENSITY;
+printf("Well, %s, your volume is %2.2f cubic feet.\n",
+name, volume);
+printf("Also, your first name has %d letters,\n",
+letters);
+printf("and we have %d bytes to store it.\n", size);
+free(name);//dynamically allocated memory is not released automatically, it must be freed
 
-struct car Honda={200,"Hondalicious"};
 
-printf("%d\n", Honda.miles);
+
 
 return 0;
 }
